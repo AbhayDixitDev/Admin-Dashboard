392aaa43d414161d9bea1064743f63e31caf2420
Abhay Dixit <dixitshivam534@gmail.com>
2024-08-10 20:57:42 +0530

    crud get operation on js data

--- product.js
+++ product.js
@@ -1,19 +1,11 @@
-window.form = () => {
-    document.querySelector('form').style.display = 'flex';
-  }
-const run=()=>{
-    let pname=document.querySelector('#pname').value;
+const run = ()=>{
+let pname=document.querySelector('#pname').value;
 let pprice=document.querySelector('#pprice').value;
 let pimage=document.querySelector('#pimage').value;
 let pbrand=document.querySelector('#pbrand').value;
 let preview=document.querySelector('#preview').value;
 let prating=document.querySelector('#prating').value;
-// console.log(pname);
-// console.log(pprice);
-// console.log(pimage);
-// console.log(pbrand);
-// console.log(preview);
-// console.log(prating);
+
 
 let product_data ={
     "product_name":pname,
@@ -33,9 +25,48 @@ let method={
     body:JSON.stringify(product_data)
 }
 fetch(url,method);
-console.log(product_data);
-// return false;
+ let selectbg = document.querySelector('#website');
+     selectbg.style.filter = "none";
+return false;
 
 }
 
+const show_form = ()=>{
+    let select = document.querySelector('#product_form');
+    select.style.display = "block";
+    let closebtn = document.querySelector('#close');
+    closebtn.style.display="block";
+    let selectbg = document.querySelector('#website');
+    selectbg.style.filter = "blur(5px)"
+}
+const Close = ()=>{
+    let select = document.querySelector('#product_form');
+    select.style.display = "none";
+    let closebtn = document.querySelector('#close');
+    closebtn.style.display="none";
+    let selectbg = document.querySelector('#website');
+    selectbg.style.filter = "none"
+}
+
+
+
+//IIFE
+(async function(){
+    let url = 'http://localhost:4000/Product';
+    let data =await fetch(url);
+    let response =await data.json();
+
+    console.log(response);
+    document.querySelector('#showproductdata').innerHTML = response.map(e=>`
+         <tr>
+         <td>${e.product_name}</td>
+         <td>${e.product_price}</td>
+         <td><img width="70px" src="${e.product_image}"></td>
+         <td>${e.product_brand}</td>
+         <td>${e.product_review}</td>
+         <td>${e.product_rating}</td>
+         </tr>
+         `).join(" ");
+
+})();
 
